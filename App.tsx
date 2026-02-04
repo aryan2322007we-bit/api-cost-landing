8961c123c9956ebb0648cae6dd9a97224e6f8b01
aryan2322007we-bit <aryan2322007we@gmail.com>
2026-02-04 19:35:10 +0530

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from "./components/NavBar";
 import Hero from './components/Hero';
 import Problem from './components/Problem';
 import Solution from './components/Solution';
